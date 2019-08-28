455679b268f0fa1031042e16845e682270634753
John McCambridge <johnmccambridge@Johns-MacBook-Pro.local>
2019-08-28 10:34:22 -0500

    add additional tooling from npm

--- src/App.js
+++ src/App.js
@@ -1,20 +1,26 @@
 const Pet = ({ name, animal, breed }) => {
-    return React.createElement("div", {}, [
-        React.createElement("h1", {}, name),
-        React.createElement("h2", {}, animal),
-        React.createElement("h2", {}, breed)
-    ]);
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, name),
+    React.createElement("h2", {}, animal),
+    React.createElement("h2", {}, breed)
+  ]);
 };
 
 const App = () => {
-    return React.createElement("div", { id: "something-importnat" }, [
-        React.createElement("h1", {}, "Adopt Me!"),
-        React.createElement(Pet, { name: "Mac", animal: "Cat", breed: "House Panther" }),
-        React.createElement(Pet, { name: "Chip", animal: "Parakeet", breed: "Cool" }),
-        React.createElement(Pet, { name: "Ava", animal: "Cat", breed: "Sleek" })
-    ]
-    );
-}
+  return React.createElement("div", { id: "something-importnat" }, [
+    React.createElement("h1", {}, "Adopt Me!"),
+    React.createElement(Pet, {
+      name: "Mac",
+      animal: "Cat",
+      breed: "House Panther"
+    }),
+    React.createElement(Pet, {
+      name: "Chip",
+      animal: "Parakeet",
+      breed: "Cool"
+    }),
+    React.createElement(Pet, { name: "Ava", animal: "Cat", breed: "Sleek" })
+  ]);
+};
 
-ReactDOM.render(React.createElement(App),
-    document.getElementById("root"));
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
